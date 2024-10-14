4152625c79297900f7ce7d2120610e0d2292964f
krystal <camelia477@outlook.com>
2024-10-14 19:26:47 +0800

    update

--- hashtable.py
+++ hashtable.py
@@ -102,7 +102,7 @@ def intersection2(list1, list2):
 
 
 # 编写一个算法来判断一个数 n 是不是快乐数。
-# 「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。如果 n 是快乐数就返回 True ；不是，则返回 False 。
+# 「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是无限循环但始终变不到 1。如果 可以变为1，那么这个数就是快乐数。如果 n 是快乐数就返回 True ；不是，则返回 False 。
 # 示例：输入：19 -> 输出：true
 # 解释：
 # 1^2 + 9^2 = 82
@@ -110,11 +110,102 @@ def intersection2(list1, list2):
 # 6^2 + 8^2 = 100
 # 1^2 + 0^2 + 0^2 = 1
 
+def get_sum(n: int) -> int:
+    new_num = 0
+    while n:
+        n, r = divmod(n, 10)
+        print(f"n={n}, r={r}")
+        new_num += r ** 2
+        print(f"new_num={new_num}")
+    return new_num
+
+
+def isHappy(n: int) -> bool:
+    record = set()
+
+    while True:
+        n = get_sum(n)
+        print(f"n={n}")
+        if n == 1:
+            return True
+
+        if n in record:
+            return False
+        else:
+            record.add(n)
+
+
+# 两数之和。给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+def two_sum(list: [int], target: int) -> list[int]:
+    """要求返回的是索引下标，而双指针法一定要排序，一旦排序之后原数组的索引就被改变了。如果要求返回的是数值，就可以用双指针法了。"""
+    records = dict()  # map用来存放访问过的元素，需要记录走之前遍历过哪些元素和对应的下标。 {key：数据元素，value：数组元素对应的下标}
+
+    for idx, num in enumerate(list):
+        if target - num in records:  # 遍历当前元素，并在map中寻找是否有匹配的key
+            return [records[target - num], idx]
+        # 如果没招到匹配对，就把访问过的元素和下标加入到map中。
+        records[num] = idx  # 将值和索引返过来存储，遍于当找到目标值时，直接通过records[value]能返回索引。
+
+    return []
+
+
+# 三数之和。
+# 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+# 示例：
+# 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+# 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+# 注意[0， 0， 0， 0] 这组数据
+def three_sum(nums: [int]) -> list[list]:
+    """
+    重点：去重逻辑的思考（要做的是不能有重复的三元组，但三元组内的元素是可以重复的）
+    a + b + c = 0 -> 双指针法：nums[i] + nums[left] + nums[right] = 0
+    """
+    result = []
+    nums.sort()  # 将列表从小到大排序，方便后续对和的值做判断
+
+    for i in range(len(nums)):
+        if nums[i] > 0:
+            return result  # 如果第一个数就大于0，那无论如何往后的三数之和都不会等于0，因此直接返回空列表
+
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue  # 确保当前的值和前一个值没有重复，确保a不重复
+
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            sums = nums[i] + nums[left] + nums[right]
+
+            if sums > 0:
+                right -= 1  # 因为是排序过的数组，如果和大于了0，那右指针就要往左移动以确保数值减小
+            elif sums < 0:
+                left += 1
+            else:
+                result.append([nums[i], nums[left], nums[right]])  # 等于0的情况，添加进result
+
+            # 确保b和c不重复
+            while left < right and nums[right] == nums[right - 1]:
+                right -= 1  # 如果右指针往左移动一位还是原数值大小的话，为了避免重复，则不重复计算，继续往左再移动
+            while left < right and nums[left] == nums[left + 1]:
+                left += 1
+
+            right -= 1
+            left += 1
+
+    return result
+
+
 if __name__ == '__main__':
     # s = "anAgram"
     # t = "nagaram"
     # print(isAnagram4(s, t))
 
-    nums1 = [4, 9, 5]
-    nums2 = [9, 4, 9, 8, 4]
-    print("final:", intersection2(nums1, nums2))
+    # nums1 = [4, 9, 5]
+    # nums2 = [9, 4, 9, 8, 4]
+    # print("final:", intersection2(nums1, nums2))
+
+    # print(isHappy(19))
+
+    # print(two_sum([1, 2, 3, 4, 5, 6], 6))
+
+    print(three_sum([-1, 0, 1, 2, -1, -4]))
